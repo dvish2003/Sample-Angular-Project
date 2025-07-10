e6f4fb4868443ef038209ff9dace644546d8183f
vishan <Vishanchathuranga2003@gmail.com>
2025-07-10 21:29:51 +0530

    commit

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,16 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import {MainComponent} from './main/main.component';
+import { Navigation } from './navigation/navigation';
+import { Content } from './content/content';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Content
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MainComponent,
-
+    Navigation,
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
---
+++ src/app/navigation/navigation.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.html',
+  styleUrl: './navigation.css'
+})
+export class Navigation {
+
+}
